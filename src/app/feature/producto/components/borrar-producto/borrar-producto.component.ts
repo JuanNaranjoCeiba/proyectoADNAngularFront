eef5a07c4fca5a24b5205802668b63fc45a017a4
Juan David Naranjo Sanchez - Ceiba Software <juan.naranjo@ceiba.com.co>
2022-02-24 08:17:22 -0500

    primer commit con la configuracion del proyecto y las paginas para listar, crear y eliminar

---
+++ src/app/feature/producto/components/borrar-producto/borrar-producto.component.ts
@@ -0,0 +1,30 @@
+import { Component, OnInit } from '@angular/core';
+import { ProductoService } from '@producto/shared/service/producto.service';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-borrar-producto',
+  templateUrl: './borrar-producto.component.html',
+  styleUrls: ['./borrar-producto.component.sass']
+})
+export class BorrarProductoComponent implements OnInit {
+  productoForm: FormGroup;
+  constructor(protected productoServices: ProductoService) { }
+
+  ngOnInit() {
+    this.construirFormularioProducto();
+  }
+
+  eliminar(){
+    this.productoServices.eliminar(this.productoForm.value).subscribe(response => {
+      console.log(response);
+    });
+  }
+
+  private construirFormularioProducto() {
+    this.productoForm = new FormGroup({
+      id: new FormControl('',[Validators.required])
+    });
+  }
+
+}
---
+++ src/app/feature/producto/components/crear-producto/crear-producto.component.ts
@@ -0,0 +1,36 @@
+import { Component, OnInit } from '@angular/core';
+import { ProductoService } from '../../shared/service/producto.service';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-crear-producto',
+  templateUrl: './crear-producto.component.html',
+  styleUrls: ['./crear-producto.component.sass']
+})
+export class CrearProductoComponent implements OnInit {
+  productoForm: FormGroup;
+  constructor(protected productoServices: ProductoService) { }
+
+  ngOnInit() {
+    this.construirFormularioProducto();
+  }
+
+  crear() {
+    this.productoServices.guardar(this.productoForm.value).subscribe(response => {
+      console.log(response)
+    });
+  }
+
+  private construirFormularioProducto() {
+    this.productoForm = new FormGroup({
+      nombre: new FormControl('',[Validators.required]),
+      precio: new FormControl('', [Validators.required]),
+      imagen: new FormControl('', [Validators.required]),
+      descripcion: new FormControl('', [Validators.required]),
+      cantidadExistencias: new FormControl('', [Validators.required]),
+      categoria: new FormControl('', [Validators.required]),
+      idUsuarioVendedor: new FormControl('', [Validators.required]),
+    });
+  }
+
+}
