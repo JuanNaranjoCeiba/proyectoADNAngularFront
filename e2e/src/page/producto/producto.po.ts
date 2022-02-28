517916b8de10cbc39669ab8d8498db38a191c5e4
Juan David Naranjo Sanchez - Ceiba Software <juan.naranjo@ceiba.com.co>
2022-02-28 12:17:28 -0500

    cambios con problema en el protactor

--- e2e/src/page/producto/producto.po.ts
+++ e2e/src/page/producto/producto.po.ts
@@ -3,8 +3,13 @@ import { by, element } from 'protractor';
 export class ProductoPage {
     private linkCrearProducto = element(by.id('linkCrearProducto'));
     private linkListarProductos = element(by.id('linkListarProducto'));
-    private inputIdProducto = element(by.id('idProducto'));
+    private inputNombreProducto = element(by.id('nombreProducto'));
+    private inputPrecioProducto = element(by.id('precioProducto'));
+    private inputImagen = element(by.id('imagenProducto'));
     private inputDescripcionProducto = element(by.id('descripcionProducto'));
+    private inputCantidadExistenciasProducto = element(by.id('cantidadExistenciasProducto'));
+    private inputCategoriaProducto = element(by.id('categoriaProducto'));
+    private inputIdUsuarioVendedor = element(by.id('idUsuarioVendedor'));
     private listaProductos = element.all(by.css('ul.productos li'));
 
     async clickBotonCrearProductos() {
@@ -15,12 +20,32 @@ export class ProductoPage {
         await this.linkListarProductos.click();
     }
 
-    async ingresarId(idProducto) {
-        await this.inputIdProducto.sendKeys(idProducto);
+    async ingresarNombre(nombreProducto) {
+        await this.inputNombreProducto.sendKeys(nombreProducto);
+    }
+
+    async ingresarPrecio(precioProducto){
+        await this.inputPrecioProducto.sendKeys(precioProducto);
+    }
+
+    async ingresarImagen(imagenProducto){
+        await this.inputImagen.sendKeys(imagenProducto);
     }
 
     async ingresarDescripcion(descripcionProducto) {
         await this.inputDescripcionProducto.sendKeys(descripcionProducto);
+    }
+
+    async ingresarCantidadExistencias(cantidadExistenciasProducto) {
+        await this.inputCantidadExistenciasProducto.sendKeys(cantidadExistenciasProducto);
+    }
+
+    async ingresarCategoria(categoriaProducto) {
+        await this.inputCategoriaProducto.sendKeys(categoriaProducto);
+    }
+
+    async ingresarIdUsuario(inputIdUsuarioVendedor){
+        await this.inputIdUsuarioVendedor.sendKeys(inputIdUsuarioVendedor);
     }
 
     async contarProductos() {
--- e2e/src/test/producto.e2e-spec.ts
+++ e2e/src/test/producto.e2e-spec.ts
@@ -15,14 +15,25 @@ describe('workspace-project Producto', () => {
     });
 
     it('Deberia crear producto', () => {
-        const ID_PRODUCTO = '001';
+        const NOMBRE_PRODUCTO = 'producto prueba';
+        const PRECIO_PRODUCTO = '100000';
+        const IMAGEN_PRODUCTO = 'imagen';
         const DESCRIPCION_PRODUCTO = 'Producto de pruebas';
+        const CANTIDADEXISTENCIAS_PRODUCTO = '8';
+        const CATEGORIA_PRODUCTO = 'prueba';
+        const ID_USUARIO_VENDEDOR = '1';
+
 
         page.navigateTo();
         navBar.clickBotonProductos();
         producto.clickBotonCrearProductos();
-        producto.ingresarId(ID_PRODUCTO);
+        producto.ingresarNombre(NOMBRE_PRODUCTO);
+        producto.ingresarPrecio(PRECIO_PRODUCTO);
+        producto.ingresarImagen(IMAGEN_PRODUCTO);
         producto.ingresarDescripcion(DESCRIPCION_PRODUCTO);
+        producto.ingresarCantidadExistencias(CANTIDADEXISTENCIAS_PRODUCTO);
+        producto.ingresarCategoria(CATEGORIA_PRODUCTO);
+        producto.ingresarIdUsuario(ID_USUARIO_VENDEDOR);
 
         // Adicionamos las validaciones despues de la creación
         // expect(<>).toEqual(<>);
